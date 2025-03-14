c76e3a54500a73ba8f6c405ddcd416e6e0a07537
EmreDH <42_emre@live.nl>
2025-03-14 15:29:58 +0100

    Register Backend opgesteld
    - Register endpoint toegevoegd
    - Wachtwoorden gehasht
    - Endpoint getest in Swagger

--- BackEnd_Course/Classes/User.cs
+++ BackEnd_Course/Classes/User.cs
@@ -6,7 +6,7 @@ public class User
     public string Email { get; set; }
     public string PasswordHash { get; set; }
     public string Role { get; set; }
-    public DateTime CreatedAt { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 
 
     public ICollection<Result> Results { get; set; }
