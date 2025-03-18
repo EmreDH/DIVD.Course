2495af4922655fa0b17423e484c6447e41e1c2a4
EmreDH <42_emre@live.nl>
2025-03-18 18:01:46 +0100

    Reset functionaliteit via e-mail
    - Implementatie van wachtwoord-reset functionaliteit
    - Generatie van wachtwoord-reset token
    - MailHog gebruikt voor het testen van de verstuurde reset e-mails
    - E-mailfunctionaliteit getest via MailHog

--- BackEnd_Course/Classes/MailService.cs
+++ BackEnd_Course/Classes/MailService.cs
@@ -5,23 +5,18 @@ public class MailService
 {
     private readonly string _smtpServer;
     private readonly int _smtpPort;
-    private readonly string _smtpUser;
-    private readonly string _smtpPass;
 
     public MailService(IConfiguration config)
     {
-        _smtpServer = config["MailSettings:SmtpServer"];
-        _smtpPort = int.Parse(config["MailSettings:SmtpPort"]);
-        _smtpUser = config["MailSettings:SmtpUser"];
-        _smtpPass = config["MailSettings:SmtpPass"];
+        _smtpServer = config["MailSettings:SmtpServer"] ?? "localhost"; 
+        _smtpPort = int.Parse(config["MailSettings:SmtpPort"] ?? "1025"); 
     }
 
     public void SendEmail(string toEmail, string subject, string body)
     {
         var message = new MimeMessage();
-
-
-        message.From.Add(new MailboxAddress("Support", _smtpUser));
+        
+        message.From.Add(new MailboxAddress("Support", "support@divd.com")); 
 
         message.To.Add(new MailboxAddress("", toEmail));
 
@@ -34,7 +29,7 @@ public class MailService
         using (var client = new SmtpClient())
         {
             client.Connect(_smtpServer, _smtpPort, false);
-            client.Authenticate(_smtpUser, _smtpPass);
+
             client.Send(message);
             client.Disconnect(true);
         }
